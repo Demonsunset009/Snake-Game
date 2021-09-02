13433fe613c27c8e7773001a4869c8cdee014201
Demonsunset009 <82150476+Demonsunset009@users.noreply.github.com>
2021-09-02 18:57:07 +0800

    Add files via upload

---
+++ SnakeGame/Snake.js
@@ -0,0 +1,98 @@
+$(document).ready(function)(){
+	// Setting up Canvas
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext("2d");
+
+	//Setting up Grids
+	var gridNum = 20;
+	var gridSize = canvas.width / gridNum;
+
+	//Setting up Player & Candy objects
+	var candy = {
+		X: 0,
+		y: 0,
+		alive: false
+	};
+	var player = {
+		x: 7,
+		y: 7,
+		//direction Right - 0, Left - 1, Up - 2, Down - 3, Stopped - 5
+		direction: 5,
+		alive: true,
+		tail: 1
+	}
+
+	//Storing the coordinates of body parts in pairs of [x, y] for each entry
+	var snakeBody = [[7, 7]];
+
+	//Examoke of snakeBody
+	var snakeBody = [
+	[7, 7], //Head is in position of x = 7, y = 7
+	[8, 7],
+	[9, 7],
+	[9, 6], //Tail is in position of x = 9, y = 6
+	];
+
+	//Setting up Keys
+	var keyPressed = null;
+	var leftKey = 37, upKey = 38, rightKey = 39, downKey = 40;
+
+	//Make a custom .insert() method for Array
+	Array.prototype.insert = function(index, item){
+		//.splice(index_to_insert, no_of_items_to_delete, new items)
+		this.splice(index, 0, item)
+	}
+};
+function update(){
+	if (keyPressed){
+		if(keyPressed == rightKey && player.direction != 1) player.direction = 0;
+		if(keyPressed = leftKey && player.direction != 0) player.direction = 1;
+		if(keyPressed = upKey && player.direction != 3) player.direction = 2;
+		if(keyPressed = downKey && player.direction != 2) player.direction = 3;
+	}
+	//Spawning Candy
+	if(!candy.alive){
+		//Generating random number form 0 to 19 (for 20 x 20 grids)
+		candy.x = Math.floor(Math.random() * gridNum);
+		candy.y = Math.floor(Math.random() * gridNum);
+
+		//Check if spawning on snake
+		var collided;
+
+		do{
+			collided = false;
+			for(var i = 0; i < player.tail; i++){
+				if(candy.x == snakeBody[i][0] && candy.y == snakeBody[i][1]){
+					collided = true;
+					candy.x = Math.floor(Math.random() * gridNum);
+					candy.y = Math.floor(Math.random() * gridNum);
+					break;
+				}
+			}
+		} white(collided);
+
+		//Now the candy is back alive!
+		candy.alive=true;
+
+		//Check if player eats the candy
+		if(player.x == candy.x && player.y == candy.y){
+			candy.alive = false;
+			player.tail++;
+		}
+		//Check if player eats itself
+		if(player.tail > 1){
+			for(var i = 1; i < player.tail; i++){
+				if(player.x == snakeBody[i][0] && player.y == snakeBody[i][1]){
+					player.alive = false;
+					clearInterval(updates);
+				}
+			}
+		}
+
+		//Check if player hits border
+		if(player.x >= gridNum || player.x < 0 || player.y >= gridNum || player.y < 0){
+			player.alive = false;
+			clearInterval(updates);
+		}
+	}
+}
